393eed5fbaea512ea6bdfe1be6305d4add3c354c
Calixte Denizet <calixte.denizet@gmail.com>
2024-05-14 09:59:06 +0200

    Don't use import.meta.url
    We ended up with a path depending on the build machine in the pdf.js bundle for Firefox (see bug 1896577).

--- compile.sh
+++ compile.sh
@@ -19,7 +19,7 @@ emcc -o ${OUTPUT}/openjpeg.js \
         -s EXPORT_NAME="'OpenJPEG'" \
         -s WASM_ASYNC_COMPILATION=0 \
         -s EXPORT_ES6=1 \
-        -s USE_ES6_IMPORT_META=1 \
+        -s USE_ES6_IMPORT_META=0 \
         -s SINGLE_FILE=1 \
         -s ENVIRONMENT='web' \
         -s ERROR_ON_UNDEFINED_SYMBOLS=1 \
@@ -34,7 +34,7 @@ emcc -o ${OUTPUT}/openjpeg.js \
         -O3 \
         -msimd128 -msse \
         --js-library ${INPUT}/myjs.js \
-        --pre-js ${INPUT}/mypre.js \
+        --pre-js ${INPUT}/mypre.js
 
 
 # -s ASSERTIONS=2 -s SAFE_HEAP=1 -s STACK_OVERFLOW_CHECK=2 -O0 -g4 \
