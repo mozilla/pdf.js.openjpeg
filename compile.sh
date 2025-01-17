9bd73aee90e476390f284b32334742ee87b29be9
Calixte Denizet <calixte.denizet@gmail.com>
2025-01-17 14:43:22 +0100

    Export a wasm file instead of embedding it in the generated js

--- compile.sh
+++ compile.sh
@@ -42,10 +42,10 @@ emcc -o ${OUTPUT}/openjpeg.js \
         -s WASM=1 \
         -s MODULARIZE=1 \
         -s EXPORT_NAME="'OpenJPEG'" \
-        -s WASM_ASYNC_COMPILATION=0 \
+        -s WASM_ASYNC_COMPILATION=1 \
         -s EXPORT_ES6=1 \
         -s USE_ES6_IMPORT_META=0 \
-        -s SINGLE_FILE=1 \
+        -s SINGLE_FILE=0 \
         -s ENVIRONMENT='web' \
         -s ERROR_ON_UNDEFINED_SYMBOLS=1 \
         -s NO_FILESYSTEM=1 \
@@ -58,9 +58,8 @@ emcc -o ${OUTPUT}/openjpeg.js \
         -flto \
         -O3 \
         -msimd128 -msse \
-        --js-library ${INPUT}/myjs.js \
-        --pre-js ${INPUT}/mypre.js
+        --js-library ${INPUT}/myjs.js
 
+chmod ugo-x ${OUTPUT}/openjpeg.wasm
 
 # -s ASSERTIONS=2 -s SAFE_HEAP=1 -s STACK_OVERFLOW_CHECK=2 -O0 -g4 \
-        
